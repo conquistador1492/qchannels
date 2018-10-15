deb97105e3b62d89fa7861271f001f36d509c255
Flammifer <37635688+Flammifer@users.noreply.github.com>
2018-10-15 18:03:17 +0300

    v1.2

--- basis.py
+++ basis.py
@@ -16,20 +16,20 @@ def rho1(qr, cr, qc):
     qc.x(qr[0])
 def rho2(qr, cr, qc):
     qc.circuit_name = "rho_A2"
-    qc.x(qr[2])
+    qc.x(qr[3])
 def rho3(qr, cr, qc):
     qc.circuit_name = "rho_B0"
     qc.h(qr[0])
 def rho4(qr, cr, qc):
     qc.circuit_name = "rho_B1"
-    qc.h(qr[2])
+    qc.h(qr[3])
 def rho5(qr, cr, qc):
     qc.circuit_name = "rho_B2"
-    qc.h(qr[2])
+    qc.h(qr[3])
     qc.x(qr[0])
-    qc.cx(qr[2], qr[1])
-    qc.cx(qr[1], qr[0])
-    qc.cx(qr[2], qr[1]) #CNOT from 3rd to 1st :)
+    qc.cx(qr[3], qr[2])
+    qc.cx(qr[2], qr[0])
+    qc.cx(qr[3], qr[2]) #CNOT from 3rd to 1st :)
 def rho6(qr, cr, qc):
     rho3(qr, cr, qc)
     qc.circuit_name = "rho_C0"
@@ -37,7 +37,7 @@ def rho6(qr, cr, qc):
 def rho7(qr, cr, qc):
     rho4(qr, cr, qc)
     qc.circuit_name = "rho_C1"
-    qs.s(qr[2])
+    qs.s(qr[3])
 def rho8(qr, cr, qc):
     rho5(qr, cr, qc)
     qc.circuit_name = "rho_C2"
