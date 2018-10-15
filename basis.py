10df62ed074724895eced740bcfef8059fd8c491
Alexey Pakhomchik <aleksey.pakhomchik@gmail.com>
2018-10-15 20:12:06 +0300

    Томография

--- basis.py
+++ basis.py
@@ -1,28 +1,27 @@
-
-import qiskit as qk
-
-import numpy as np
-import argparse
-qr = qk.QuantumRegister(4)
-cr = qk.ClassicalRegister(4)
-qc = qk.QuantumCircuit(qr, cr)
-
-basis = []
 def rho0(qr, cr, qc):
     qc.circuit_name = "rho_A0" #where A and 0 stand for type of basics element
+
 
 def rho1(qr, cr, qc):
     qc.circuit_name = "rho_A1"
     qc.x(qr[0])
+
+
 def rho2(qr, cr, qc):
     qc.circuit_name = "rho_A2"
     qc.x(qr[3])
+
+
 def rho3(qr, cr, qc):
     qc.circuit_name = "rho_B0"
     qc.h(qr[0])
+
+
 def rho4(qr, cr, qc):
     qc.circuit_name = "rho_B1"
     qc.h(qr[3])
+
+
 def rho5(qr, cr, qc):
     qc.circuit_name = "rho_B2"
     qc.h(qr[3])
@@ -30,25 +29,24 @@ def rho5(qr, cr, qc):
     qc.cx(qr[3], qr[2])
     qc.cx(qr[2], qr[0])
     qc.cx(qr[3], qr[2]) #CNOT from 3rd to 1st :)
+
+
 def rho6(qr, cr, qc):
     rho3(qr, cr, qc)
     qc.circuit_name = "rho_C0"
-    qs.s(qr[0])
+    qc.s(qr[0])
+
+
 def rho7(qr, cr, qc):
     rho4(qr, cr, qc)
     qc.circuit_name = "rho_C1"
-    qs.s(qr[3])
+    qc.s(qr[3])
+
+
 def rho8(qr, cr, qc):
     rho5(qr, cr, qc)
     qc.circuit_name = "rho_C2"
-    qs.s(qr[0])
+    qc.s(qr[0])
 
-basis.append(rho0)
-basis.append(rho1)
-basis.append(rho2)
-basis.append(rho3)
-basis.append(rho4)
-basis.append(rho5)
-basis.append(rho6)
-basis.append(rho7)
-basis.append(rho8)
+variables = globals()
+QUTRIT_BASIS_FUNC = [variables['rho' + str(i)] for i in range(9)]
--- master_experiment.py
+++ master_experiment.py
@@ -3,6 +3,7 @@
 
 from Qconfig import config
 from qiskit import QuantumRegister, ClassicalRegister, execute, IBMQ
+from qiskit import QuantumCircuit
 
 from qiskit.tools.qcvv.tomography import *
 from tools import chunks, MAX_JOBS_PER_ONE
@@ -11,6 +12,7 @@ import numpy as np
 import argparse
 
 from landau import LandauCircuit
+from basis import QUTRIT_BASIS_FUNC
 
 parser = argparse.ArgumentParser(description='Run experiments on IBM computers')
 parser.add_argument('-n', '--num-token', type=int, default=0, help='Number of token from Qconfig.py')
@@ -29,25 +31,31 @@ IBMQ.enable_account(APItoken, **config)
 backend = next(filter(lambda backend: backend.configuration().get('name') == args.backend, IBMQ.backends()))
 shots = args.shots
 
-np.set_printoptions(threshold=np.nan)
-
 num_qubits = 5
+circuit_name = 'landau'
+np.set_printoptions(threshold=np.nan)
 q = QuantumRegister(num_qubits)
 c = ClassicalRegister(num_qubits)
 
-circuit_name = 'landau'
-qc = LandauCircuit(q, c, name=circuit_name,
-                   coupling_map=backend.configuration()['coupling_map'])
-qc.circuit_name = circuit_name
+measure_basis = [0, 3]
+tomo_set = tomography_set(measure_basis)
+number_measure_experiments = 3**len(measure_basis)
 
-tomo_set = tomography_set([0, 3])
-circuits = create_tomography_circuits(qc, q, c, tomo_set)
+jobs = []
+for rho_f in QUTRIT_BASIS_FUNC:
+    qc = QuantumCircuit(q, c)
+    rho_f(q, c, qc)
+    qc += LandauCircuit(q, c, name=circuit_name,
+                        coupling_map=backend.configuration()['coupling_map'])
+    qc.circuit_name = circuit_name
+    circuits = create_tomography_circuits(qc, q, c, tomo_set)
+    jobs.extend(circuits)
 
 res = None
-for i, part_circuits in enumerate(chunks(circuits, MAX_JOBS_PER_ONE)):
+for i, part_jobs in enumerate(chunks(jobs, MAX_JOBS_PER_ONE)):
     print(f'chunk number: {i + 1}')
     execute_kwargs = {
-        'circuits': part_circuits,
+        'circuits': part_jobs,
         'backend': backend,
         'shots': shots,
         'max_credits': 15
@@ -58,6 +66,7 @@ for i, part_circuits in enumerate(chunks(circuits, MAX_JOBS_PER_ONE)):
     else:
         res += new_res
 
+matrices = []
+for res_chunk in chunks(res, number_measure_experiments):
+    matrices.append(fit_tomography_data(tomography_data(res, qc.name, tomo_set)))
 
-density_matrix = fit_tomography_data(tomography_data(res, qc.name, tomo_set))
-print(f"[{density_matrix}]")
