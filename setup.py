f257c74f3e76b57fb75e27e4de7e7b7705d09818
Alexey Pakhomchik <aleksey.pakhomchik@gmail.com>
2018-12-11 04:37:29 +0300

    Deleted requirements.py and removed deprecated dependency

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(name='qchannels',
       license='MIT',  # TODO
       packages=find_packages(exclude=['test*']),
       install_requires=[
-          'tqdm>=4.25.0',
           'qiskit==0.6.1',
           'jsonschema>=2.6.0',
           'psutil>=5.4.7'
